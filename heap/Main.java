8d0346b64b18044ef7dbe6de6c079ca44dfd19f7
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2023-04-02 09:29:33 +0700

    heap

---
+++ heap/Main.java
@@ -0,0 +1,49 @@
+public class Main {
+  public static void main(String[] args) {
+    Heap heap = new Heap(15);
+    heap.insert(78);
+    heap.displayHeap();
+
+    heap.insert(3);
+    heap.displayHeap();
+
+    heap.insert(9);
+    heap.displayHeap();
+
+    heap.insert(10);
+    heap.displayHeap();
+
+    heap.insert(23);
+    heap.displayHeap();
+
+    heap.insert(77);
+    heap.displayHeap();
+
+    heap.insert(34);
+    heap.displayHeap();
+
+    heap.insert(86);
+    heap.displayHeap();
+
+    heap.insert(90);
+    heap.displayHeap();
+
+    heap.insert(100);
+    heap.displayHeap();
+
+    heap.insert(20);
+    heap.displayHeap();
+
+    heap.insert(66);
+    heap.displayHeap();
+
+    heap.insert(94);
+    heap.displayHeap();
+
+    heap.insert(63);
+    heap.displayHeap();
+
+    heap.insert(97);
+    heap.displayHeap();
+  }
+}
